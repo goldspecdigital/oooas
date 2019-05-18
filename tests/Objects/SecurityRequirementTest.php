9845759b7f5fa58b4656b14c4f61d980dc99e412
Matthew Inamdar <matt@goldspecdigital.com>
2019-05-18 22:50:19 +0100

    Added test for SecurityRequirement object

---
+++ tests/Objects/SecurityRequirementTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GoldSpecDigital\ObjectOrientedOAS\Tests\Objects;
+
+use GoldSpecDigital\ObjectOrientedOAS\Objects\SecurityRequirement;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\SecurityScheme;
+use GoldSpecDigital\ObjectOrientedOAS\OpenApi;
+use GoldSpecDigital\ObjectOrientedOAS\Tests\TestCase;
+
+class SecurityRequirementTest extends TestCase
+{
+    /** @test */
+    public function create_with_all_parameters_works()
+    {
+        $securityScheme = SecurityScheme::create();
+
+        $securityRequirement = SecurityRequirement::create()
+            ->name('OAuth2')
+            ->securitySchemes($securityScheme);
+
+        $openApi = OpenApi::create()
+            ->security($securityRequirement);
+
+        $this->assertEquals([
+            'security' => [
+                ['OAuth2' => []],
+            ],
+        ], $openApi->toArray());
+    }
+}
