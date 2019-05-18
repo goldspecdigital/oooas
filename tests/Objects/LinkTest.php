46f5d82d2708c1b81bc05c2d538a7d0a3ef374a1
Matthew Inamdar <matt@goldspecdigital.com>
2019-05-18 21:15:22 +0100

    Added test for Link object

---
+++ tests/Objects/LinkTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GoldSpecDigital\ObjectOrientedOAS\Tests\Objects;
+
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Link;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Response;
+use GoldSpecDigital\ObjectOrientedOAS\Tests\TestCase;
+
+class LinkTest extends TestCase
+{
+    /** @test */
+    public function create_with_all_parameters_works()
+    {
+        $link = Link::create()
+            ->name('LinkName')
+            ->href('https://goldspecdigital.com')
+            ->operationId('goldspecdigital')
+            ->description('The GoldSpec Digital website');
+
+        $response = Response::create()
+            ->links($link);
+
+        $this->assertEquals([
+            'links' => [
+                'LinkName' => [
+                    'href' => 'https://goldspecdigital.com',
+                    'operationId' => 'goldspecdigital',
+                    'description' => 'The GoldSpec Digital website',
+                ],
+            ],
+        ], $response->toArray());
+    }
+}
