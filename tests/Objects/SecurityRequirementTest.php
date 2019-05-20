3d2a942e8d9e675eedeb4d481717eafa38348ed7
Matt Inamdar <matt@ayup.agency>
2019-05-20 13:23:26 +0100

    Added test for SecurityRequirement with scopes defined

--- tests/Objects/SecurityRequirementTest.php
+++ tests/Objects/SecurityRequirementTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace GoldSpecDigital\ObjectOrientedOAS\Tests\Objects;
 
+use GoldSpecDigital\ObjectOrientedOAS\Objects\OAuthFlow;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\SecurityRequirement;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\SecurityScheme;
 use GoldSpecDigital\ObjectOrientedOAS\OpenApi;
@@ -13,6 +14,31 @@ class SecurityRequirementTest extends TestCase
 {
     /** @test */
     public function create_with_all_parameters_works()
+    {
+        $oauthFlow = OAuthFlow::create()
+            ->flow(OAuthFlow::FLOW_CLIENT_CREDENTIALS)
+            ->scopes(['read:user' => 'Access all user info']);
+
+        $securityScheme = SecurityScheme::create('OAuth2')
+            ->type(SecurityScheme::TYPE_OAUTH2)
+            ->flows($oauthFlow);
+
+        $securityRequirement = SecurityRequirement::create()
+            ->securityScheme($securityScheme)
+            ->scopes('read:user');
+
+        $openApi = OpenApi::create()
+            ->security($securityRequirement);
+
+        $this->assertEquals([
+            'security' => [
+                ['OAuth2' => ['read:user']],
+            ],
+        ], $openApi->toArray());
+    }
+
+    /** @test */
+    public function create_with_no_scopes_works()
     {
         $securityScheme = SecurityScheme::create('OAuth2');
 
