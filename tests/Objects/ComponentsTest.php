9605c7bb3d5c9c7599d9575f422cf244e3470c9f
Matt Inamdar <matt@goldspecdigital.com>
2019-06-21 22:56:46 +0100

    feature/support-all-components (#26)
    * Added other components
    
    * Added full petstore expanded tests with components
    
    * Updated README and added test for new example

--- tests/Objects/ComponentsTest.php
+++ tests/Objects/ComponentsTest.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace GoldSpecDigital\ObjectOrientedOAS\Tests\Objects;
 
 use GoldSpecDigital\ObjectOrientedOAS\Objects\Components;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Example;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Header;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Link;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\OAuthFlow;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Parameter;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\RequestBody;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Response;
+use GoldSpecDigital\ObjectOrientedOAS\Objects\Schema;
 use GoldSpecDigital\ObjectOrientedOAS\Objects\SecurityScheme;
 use GoldSpecDigital\ObjectOrientedOAS\Tests\TestCase;
 
@@ -14,19 +21,68 @@ class ComponentsTest extends TestCase
     /** @test */
     public function create_with_all_parameters_works()
     {
+        $schema = Schema::object('ExampleSchema');
+
+        $response = Response::created('ResourceCreated');
+
+        $parameter = Parameter::query('Page')
+            ->name('page');
+
+        $example = Example::create('PageExample')
+            ->value(5);
+
+        $requestBody = RequestBody::create('CreateResource');
+
+        $header = Header::create('HeaderExample');
+
         $oauthFlow = OAuthFlow::create()
             ->flow(OAuthFlow::FLOW_IMPLICIT)
             ->authorizationUrl('http://example.org/api/oauth/dialog');
 
-        $securityScheme = SecurityScheme::create()
-            ->objectId('OAuth2')
+        $securityScheme = SecurityScheme::create('OAuth2')
             ->type(SecurityScheme::TYPE_OAUTH2)
             ->flows($oauthFlow);
 
+        $link = Link::create('LinkExample');
+
         $components = Components::create()
-            ->securitySchemes($securityScheme);
+            ->schemas($schema)
+            ->responses($response)
+            ->parameters($parameter)
+            ->examples($example)
+            ->requestBodies($requestBody)
+            ->headers($header)
+            ->securitySchemes($securityScheme)
+            ->links($link);
 
         $this->assertEquals([
+            'schemas' => [
+                'ExampleSchema' => [
+                    'type' => 'object',
+                ],
+            ],
+            'responses' => [
+                'ResourceCreated' => [
+                    'description' => 'Created',
+                ],
+            ],
+            'parameters' => [
+                'Page' => [
+                    'name' => 'page',
+                    'in' => 'query',
+                ],
+            ],
+            'examples' => [
+                'PageExample' => [
+                    'value' => 5,
+                ],
+            ],
+            'requestBodies' => [
+                'CreateResource' => [],
+            ],
+            'headers' => [
+                'HeaderExample' => [],
+            ],
             'securitySchemes' => [
                 'OAuth2' => [
                     'type' => 'oauth2',
@@ -36,6 +92,9 @@ class ComponentsTest extends TestCase
                         ],
                     ],
                 ],
+            ],
+            'links' => [
+                'LinkExample' => [],
             ],
         ], $components->toArray());
     }
